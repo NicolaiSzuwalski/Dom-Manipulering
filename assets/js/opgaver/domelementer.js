6e01834aacbcfb5076f9b01bd5c4bacd6dd63e71
NicolaiSzuwalski <132432642+NicolaiSzuwalski@users.noreply.github.com>
2023-09-25 13:00:32 +0200

    Done

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -8,7 +8,10 @@ Du skal finde elementet med id="yellowFigure" og console logge det.
 */
 
 // din kode her
-
+const red = document.getElementById('redFigure')
+const yellow = document.getElementById('yellowFigure')
+console.log(red)
+console.log(yellow)
 
 /* opgave 1.2
 Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
@@ -16,7 +19,12 @@ farven på elementerne skal være blå. brug evt. elelement.style.backgroundColo
 */
 
 
+
+
 // din kode her
+
+red.style.backgroundColor = 'blue';
+yellow.style.backgroundColor = 'blue';
 
 
 
@@ -29,6 +37,13 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 
 // din kode her
 
+const opgaveTwo = document.getElementById('opgaveTwo');
+
+let nyh2 = document.createElement('h2')
+nyh2.innerText = 'Jeg har løst opgave 2.1'
+
+opgaveTwo.appendChild(nyh2);
+
 
 // opgave 3 liveHTML lists
 /* opgave 3.1
@@ -36,6 +51,9 @@ Du skal finde alle elementer med klassen purpleFigures og console logge resultat
 */
 
 // din kode her
+
+const purpleFigures = document.getElementsByClassName('purpleFigures');
+console.log(purpleFigures)
 
 
 /* opgave 3.2
@@ -46,6 +64,13 @@ array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScr
 
 // din kode her
 
+let nytArray = Array.from(purpleFigures)
+console.log(nytArray)
+
+nytArray.map(element =>{
+    element.style.backgroundColor = 'red'
+});
+
 
 /* opgave 3.3
 Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
@@ -53,7 +78,13 @@ myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 */
 
 
+
 // din kode her
+nytArray.map(element =>{
+    element.innerText = 'red'
+});
+
+
 
 
 
@@ -68,6 +99,41 @@ const myData = {
     image: 'assets/img/standard_tiger.jpg',
     description: 'En tiger er et stort, katteagtig rovdyr med orange og sort striber i pelsen og skarpe tænder og kløer.'
 };
+
+
+const opgaveFour = document.getElementById('opgaveFour');
+console.log(opgaveFour)
+
+myCardContainer = document.createElement('h2');
+myCardContainer.innerText = myData.name
+
+
+const h2 = document.createElement('h2');
+const img = document.createElement('img');
+const figcaption = document.createElement('figcaption');
+
+console.log(myData)
+const createCard = h2.innerText = myData.name
+const createCard2 = img.src = myData.image
+const createCard3 = figcaption.innerText = myData.description
+
+h2.style.textTransform = 'capitalize'
+img.style.width = '100%'
+
+
+
+opgaveFour.appendChild(h2);
+opgaveFour.appendChild(img);
+opgaveFour.appendChild(figcaption);
+
+
+
+
+
+
+
+
+
 
 
 // din kode her
